c58427fafe1a99fd100a1378a41a3a62fa6a53d0
j130617 <39986473+j130617@users.noreply.github.com>
2019-03-11 16:28:40 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,3 +1,5 @@
-/*var googleDocURL = 'https://docs.google.com/spreadsheets/d/1HlindMPfrk1y17ZkPbQRUmLU07tDiWry92AkFWlsCNI/edit#gid=0';*/
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vScAAX4tmSA3A9No9JZs6_3QrtLPj887w5xSp8QyEodVk4TKN2WIzIjSoMNHNx3_fX2LwpqprsFNWKW/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1HlindMPfrk1y17ZkPbQRUmLU07tDiWry92AkFWlsCNI/edit#gid=0';
+
+
+/*var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vScAAX4tmSA3A9No9JZs6_3QrtLPj887w5xSp8QyEodVk4TKN2WIzIjSoMNHNx3_fX2LwpqprsFNWKW/pubhtml';*/
 var googleURL = 'https://www.google.com';
